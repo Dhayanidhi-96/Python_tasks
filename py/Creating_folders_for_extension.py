74fd45fdf749c724e16ae648f3aa0bca7f2e90d1
DHAYANIDHI P <144535675+Dhayanidhi-96@users.noreply.github.com>
2025-03-15 08:11:36 +0530

    Initial commit of file sorter script

---
+++ py/Creating_folders_for_extension.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+folder_path = "D:\Career\Python task"
+
+for file_name in os.listdir(folder_path):
+    file_path = os.path.join(folder_path,file_name)
+    if os.path.isfile(file_path):
+        _, extension = os.path.splitext(file_name)
+        extension = extension[1:]
+
+        if extension:
+            new_folder_path = os.path.join(folder_path,extension)
+            if not os.path.exists(new_folder_path):
+                os.makedirs(new_folder_path)
+            shutil.move(file_path,os.path.join(new_folder_path,file_name))
+print("Files have been sorted by extension!")
